b6e486b9dabf0864f0e7e7aebf369e1081e492b2
admin <eeoneguifeed@gmail.com>
2023-05-20 20:39:10 +0600

    Node constructor added.

--- src/BST.java
+++ src/BST.java
@@ -5,5 +5,10 @@ public class BST <K extends Comparable<K>,V> {
         private V val;
         private Node left, right;
 
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+            left = right = null;
+        }
     }
 }
