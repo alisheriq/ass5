1eff7ce2b7ae396693a6e5f6fea6cf152183eb0d
admin <eeoneguifeed@gmail.com>
2023-05-20 20:49:57 +0600

    put(), get() methods tested.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        BST bst = new BST();
+        bst.put(10, "Sample1");
+        bst.put(5, "Sample2");
+        bst.put(15, "Sample3");
+        bst.put(3, "Sample4");
+        bst.put(8, "Sample5");
+        bst.put(13, "Sample6");
+        bst.put(18, "Sample7");
+        System.out.println(bst.get(10));
+        System.out.println(bst.get(5));
+        System.out.println(bst.get(15));
+        System.out.println(bst.get(3));
+        System.out.println(bst.get(8));
+        System.out.println(bst.get(13));
+        System.out.println(bst.get(18));
     }
 }
